b340efd5d32f9e9febf724a1d2cf95dd9d4785bc
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2021-09-28 15:40:38 -0500

    feat: add createHolidays module

---
+++ src/services/createHolidays.js
@@ -0,0 +1,53 @@
+/**
+ * @file Manages createHolidays module, used to create nonworkdays property the
+ * class Month.
+ */
+
+// ━━	FUNCTIONS	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `mergePlans()` function returns an object array with the activities of
+ * the month, requires a parameter that must be an object, with the properties
+ * `tasks`, `events`, `appointments` and `meetings`.
+ *
+ * @param     {object} planner - An `object` with month activities ordered by type.
+ * @returns   {Array.<object>} An array with datebookItem.
+ * @private
+ * @example const datebook = mergePlans({
+ *   tasks: [],
+ *   events: [],
+ *   appointments: [],
+ *   meetings: [],
+ * }); // expected value []
+ *
+ */
+const mergePlans = planner => [
+  ...planner.tasks,
+  ...planner.events,
+  ...planner.appointments,
+  ...planner.meetings,
+];
+
+// ━━	MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `createHolidays()` function returns a number array. The value of the
+ * returned elements is the same `Date.prototype.getDate()`, that represent the
+ * month's days that it is holidays.
+ *
+ * The function requires a parameter that must be an object, with the properties
+ * `tasks`, `events`, `appointments` and `meetings`.
+ *
+ * @param     {object} planner - Object with sorted datebookitems by type.
+ * @returns   {Array.<number>} An array with numeric items.
+ * @private
+ * @example const holidays = getDaysHoliday(datebook); // [1,2,3]
+ *
+ */
+const createHolidays = planner => {
+  const merged = mergePlans(planner);
+  if (merged.length === 0) return [];
+  const holidays = merged.reduce((a, i) => (i.holiday ? [...a, i.YYMMDD[2]] : [...a]), []);
+  return Array.from(new Set(holidays));
+};
+
+// ━━	EXPORT MODULE	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+module.exports = createHolidays;
